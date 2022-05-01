a6ce16ca68ed7a8c6a2d5d03d1ab2f8fefd373de
Eric Meinzer <emeinzer7@gmail.com>
2022-05-01 00:32:22 -0500

    migrations issues with the startup.cs file; progress save

--- Startup.cs
+++ Startup.cs
@@ -27,18 +27,40 @@ namespace event_log
             // Add framework services.
             // see if we have an environment variable
             string DATABASE_URL = Environment.GetEnvironmentVariable("DATABASE_URL_STR");
+            Console.WriteLine($"--- DATABASE_URL: {DATABASE_URL} --- ");
+
             string connectionString = (DATABASE_URL == null ? Configuration.GetConnectionString("DefaultConnection") : DATABASE_URL);
             Console.WriteLine($"Using connection string: {connectionString}");
+
+
+            Console.WriteLine("--- the fuck --- " + services.AddDbContext<ApplicationContext>(options =>
+                            options.UseNpgsql(connectionString)
+                        ));
 
             services.AddDbContext<ApplicationContext>(options =>
                 options.UseNpgsql(connectionString)
             );
+            Console.WriteLine($"-- end the fuck -- ");
+
+
+            System.Console.WriteLine("--- and here --- " + services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 
+            Console.WriteLine($"-- end here -- ");
+
+
+            // --- LOOK INTO THIS --- this is not running and program skipped over it --- // 
+
             // In production, the React files will be served from this directory
-            services.AddSpaStaticFiles(configuration => {
+            services.AddSpaStaticFiles(configuration =>
+            {
+                System.Console.WriteLine(" we get inside of AddSpaStaticFiles ");
                 configuration.RootPath = "ClientApp/build";
+                System.Console.WriteLine(" we exit ");
             });
+
+            Console.WriteLine($"-- end whattabout this -- ");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
